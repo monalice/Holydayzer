8719ecc882b3bb3853c7f22860b7f4a775610a09
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-18 14:30:40 -0300

    interact with Holydayzer class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Holydayzer holiday = new Holydayzer();
+
+        System.out.println("O melhor feriado: ");
+        holiday.getHoliday("21/04/2023");
+
+        System.out.println("Todos os feriados: ");
+        holiday.getAll();
     }
 }
